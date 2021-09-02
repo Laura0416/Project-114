731fb2d490c2525dc1e6a840b2c7406da13509ae
Laura0416 <77352015+Laura0416@users.noreply.github.com>
2021-09-01 20:58:53 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+    console.log("hellow");
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+    
+}
+
+function take_snapshot(){
+    save("myImage.png");
+}
